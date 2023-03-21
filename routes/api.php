7d878f37acf908edf693a29550e5e0ca73dc5dca
panicADDICTED <90740739+panicADDICTED@users.noreply.github.com>
2023-03-20 21:05:23 -0600

    add login client

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
 Route::get('/login-movil/{email}', [AuthController::class, 'loginMovil']);
-
+Route::get('/login-movil-client/{email}', [AuthController::class, 'loginMovilClient']);
 //USER
 Route::resource('users', UsersController::class);
 Route::get('/all_clients', [UsersController::class, 'showClients']);
